0d99a8759734de757601c7d2790e670927c7e52f
Vinicius <vinicius.sousa.96@outlook.com>
2017-01-23 01:59:22 -0200

    Adicionando parâmetros ao módulo

--- exemplos/module_math.rb
+++ exemplos/module_math.rb
@@ -1,12 +1,30 @@
 
 module MyModule
-  def MyModule.a
-    puts "resultado #{self}"
+  def self.a
     puts "Method 'a' from MyModule"
   end
 
-  def b
-    puts "resultado #{self/ 1}"
+  def indo(value)
+    for i in self.to_i..value.to_i
+        puts "resultado #{i}"
+    end
+    x = self.to_i + 1
+    puts "soma #{x}"
     puts "Instance method 'b' from MyModule"
   end
 end
+
+String.include MyModule # => String
+"1".indo("10")
+
+#=> resultado 1
+#=> resultado 2
+#=> resultado 3
+#=> resultado 4
+#=> resultado 5
+#=> resultado 6
+#=> resultado 7
+#=> resultado 8
+#=> resultado 9
+#=> resultado 10
+#=> soma 2
